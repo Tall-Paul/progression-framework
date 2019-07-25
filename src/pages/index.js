3f7016a27905d02977ba3d8122edc2e9b51c3e41
Paul Berry <Paul Berry@freeway.local>
2019-07-25 13:57:30 +0100

    updated homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,50 +24,13 @@ export default class Index extends React.Component<Props> {
       <Layout data={this.props.data}>
         <DescriptionView>
           <DescriptionTitleGroup>
-            <Subtitle>Welcome To</Subtitle>
-            <Title>Progression at Monzo 🎉</Title>
+            <Title>Freeway Progression Framework</Title>
           </DescriptionTitleGroup>
           <Description>
-            <DescriptionText>
-              This is where we keep the progression frameworks we use at Monzo.
-            </DescriptionText>
-            <DescriptionText>
-              Progression frameworks are tools that help us evaluate and pay our
-              staff consistently, and help people understand how to progress in
-              their work.
-            </DescriptionText>
-            <DescriptionText>
-              We&apos;ve put them here for everyone at Monzo (and anyone in the
-              world!) to use.
-            </DescriptionText>
-            <DescriptionText>
-              We&apos;ll be adding more frameworks as we build them, and
-              updating existing ones as we grow, so expect things to change
-              around here!
-            </DescriptionText>
-            <DescriptionText>
-              This site, along with all our progression frameworks, are open
-              source. That means you can contribute to the development of this
-              site or our progression frameworks, and use both as inspiration
-              for your own projects!
-            </DescriptionText>
-            <DescriptionText>
-              To get involved, go to{' '}
-              <a href="https://github.com/monzo/progression-framework">
-                <FontAwesomeIcon icon={faGithub} />{' '}
-                @monzo/progression-framework.
-              </a>
-            </DescriptionText>
-            <DescriptionText>
-              You can find out more about progression at Monzo and what it means
-              for us in this{' '}
-              <a href="https://monzo.com/blog/2019/01/07/progression">
-                blog post.
-              </a>
-            </DescriptionText>
-            <DescriptionText>
-              Take a look around and let us know what you think! 🚀
-            </DescriptionText>
+             <DescriptionText>This is a preview of the Progression framework in use at Freeway insurance.</DescriptionText>
+             <DescriptionText>It's based on the excellent Monzo Progression Framework: <a href="https://github.com/monzo/progression-framework">Here</a></DescriptionText>
+             <DescriptionText>Source code is <a href="https://github.com/Tall-Paul/progression-framework">Here</a> </DescriptionText>
+             <DescriptionText>Deployments to the Master Branch are automatically built and deployed on netlify: <img src="https://api.netlify.com/api/v1/badges/fa5ab270-44ed-42d9-bea9-d46e8d0ef7d0/deploy-status"></img></DescriptionText>
           </Description>
         </DescriptionView>
       </Layout>

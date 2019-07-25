bcfa0e8764c794681444ad5735026dc1b3d9e5db
Paul Berry <Paul Berry@freeway.local>
2019-07-25 15:05:36 +0100

    security policy change for remote images

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,9 @@
-const ReactGA = require('react-ga')
+/*const ReactGA = require('react-ga')
 
 ReactGA.initialize('UA-63104269-2')
 ReactGA.set({
   appName: 'Progression at Monzo'
-})
+})*/
 
 exports.onRouteUpdate = (state) => {
   ReactGA.pageview(state.location.pathname)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Monzo Progression',
+    title: 'Freeway Progression',
   },
   plugins: [
     `gatsby-plugin-styled-components`,
